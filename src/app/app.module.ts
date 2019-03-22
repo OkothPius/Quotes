d99915e8ed6e516f379505b55f52a06b67075ffd
OkothPius <orukopius8@gmail.com>
2019-03-22 17:08:36 +0300

    Save all the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    let today:Date = new Date(); //get currentr date and nativeElement
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(value-todayWithNoTime) //returns value in milliseconds
+    const secondsInADay= 86400; //60sec*60min in an hour *24hrs
+
+    var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
+
+    var dateCounter=dateDifferenceSeconds/secondsInADay;
+
+    if (dateCounter >= 1 && value > todayWithNoTime){
+      return dateCounter;
+    }else{
+      return 0;
+    }
+
+  }
+
+}
