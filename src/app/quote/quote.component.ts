57027cf31c5c610f58985d07c0d599a534ccd761
OkothPius <orukopius8@gmail.com>
2019-03-23 16:39:51 +0300

    Adding form validation

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -20,8 +20,7 @@ export class QuoteComponent implements OnInit {
   ]
   deleteQuote(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want
-        to delete ${this.quotes[index].name}`)
+      let toDelete = confirm(`Are you sure you wantto delete ${this.quotes[index].name}`)
 
       if (toDelete) {
         this.quotes.splice(index, 1);
@@ -30,9 +29,15 @@ export class QuoteComponent implements OnInit {
   }
 
   toogleDetails(index) {
-    this.quotes[index].showDescription = !this.quotes
-    [index].showDescription;
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id=quoteLength + 1;
+    quote.completeDate =new Date (quote.completeDate)
+    this.quotes.push(quote)
+  }
+
 
   constructor() { }
   ngOnInit() {
